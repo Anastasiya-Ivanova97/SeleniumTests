3566e479acb1d2e6bf0b1bf0814d6c17be9a71cd
Anastasiya-Ivanova97 <akaionna@mail.ru>
2019-09-26 11:46:41 +0400

    Commit 1

---
+++ JavaHomeworkJSON/src/CompanyType.java
@@ -0,0 +1,11 @@
+public class CompanyType {
+    private int id;
+    private String name_short;
+    private String name_full;
+
+    public CompanyType(int id, String name_short, String name_full) {
+        this.id = id;
+        this.name_short = name_short;
+        this.name_full = name_full;
+    }
+}
