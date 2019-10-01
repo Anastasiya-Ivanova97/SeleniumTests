6b06bb3402379217908cd49288e907ef1ca4b41a
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-01 22:06:03 +0300

    Update WebDriverSettings.java

--- tests/src/test/java/WebDriverSettings.java
+++ tests/src/test/java/WebDriverSettings.java
@@ -2,27 +2,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class WebDriverSettings {
     public ChromeDriver driver;
 
     @Before
     public void setUp() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String path = br.readLine();
-            System.setProperty("webdriver.chrome.driver", path);
+            System.setProperty("webdriver.chrome.driver", path); //здесь будет обозначен путь до драйвера
             driver = new ChromeDriver();
-        } catch (FileNotFoundException f) {
-            System.out.println("Введите еще раз путь до файла");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+         }
 
     @After
     public void close() {
